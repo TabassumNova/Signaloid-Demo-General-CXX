a536892bd1bf80829afff917221e9dffea0c0ffb
TabassumNova <77242943+TabassumNova@users.noreply.github.com>
2023-12-07 14:59:34 +0600

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,62 @@
 // main(int argc, char *  argv[])
 main()
 {
+<<<<<<< HEAD
+	std::cout << "I am writing to express my interest in the Machine Learning Engineer Robotics at TruPhysics. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << "diff" << std::endl;
+=======
 	std::cout << "I am writing to express my interest in the Machine Learning Engineer Robotics at TruPhysics. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
 // 	std::vector<double> vec {
 // 		UxHwDoubleUniformDist(0.0, 1.0),
 // 		UxHwDoubleUniformDist(1.0, 2.0),
 // 	};
+>>>>>>> adbc93f9af45dd137b5b3219a2fa899380f7fe62
 
+<<<<<<< HEAD
+// 	std::vector<double> vec {
+// 		UxHwDoubleUniformDist(0.0, 1.0),
+// 		UxHwDoubleUniformDist(1.0, 2.0),
+// 	};
+=======
 // 	std::cout << "Initial distributions:" << std::endl;
 // 	for (const auto& dist : vec)
 // 	{
 // 		std::cout << dist << std::endl;
 // 	}
+>>>>>>> adbc93f9af45dd137b5b3219a2fa899380f7fe62
 
+<<<<<<< HEAD
+// 	std::cout << "Initial distributions:" << std::endl;
+// 	for (const auto& dist : vec)
+// 	{
+// 		std::cout << dist << std::endl;
+// 	}
+=======
 // 	std::adjacent_difference(vec.begin(), vec.end(), vec.begin());
+>>>>>>> adbc93f9af45dd137b5b3219a2fa899380f7fe62
 
+<<<<<<< HEAD
+// 	std::adjacent_difference(vec.begin(), vec.end(), vec.begin());
+=======
 // 	std::cout << "Differences:" << std::endl;
 // 	for (const auto& diff : vec)
 // 	{
 // 		std::cout << diff << std::endl;
 // 	}
+>>>>>>> adbc93f9af45dd137b5b3219a2fa899380f7fe62
+
+<<<<<<< HEAD
+// 	std::cout << "Differences:" << std::endl;
+// 	for (const auto& diff : vec)
+// 	{
+// 		std::cout << diff << std::endl;
+// 	}
+=======
+// #ifdef DEBUG
+// 	std::cout << "debug message" << std::endl;
+// #endif
+>>>>>>> adbc93f9af45dd137b5b3219a2fa899380f7fe62
 
 // #ifdef DEBUG
 // 	std::cout << "debug message" << std::endl;

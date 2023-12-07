3f9676e75d3faf75a9f482b18eea032f59475796
TabassumNova <77242943+TabassumNova@users.noreply.github.com>
2023-12-07 15:59:01 +0600

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,25 @@
 // main(int argc, char *  argv[])
 main()
 {
-	std::cout << "I am writing to express my interest in the Machine Learning Engineer Robotics at TruPhysics. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
+	std::cout << "I am writing to express my interest in the Software Engineer, Probabilistic Robotics Applications (DE) at Signaloid. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
 	std::cout << '\n' << std::endl;
-	std::cout << "diff" << std::endl;
-	std::cout << "I am writing to express my interest in the Machine Learning Engineer Robotics at TruPhysics. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
-// 	std::vector<double> vec {
+	std::cout << "I completed my graduation in Electrical and Electronic Engineering from Bangladesh University of Engineering and Technology (BUET) in 2018. Because of my keen interest in machine learning and data analysis, I have started my master’s in Medical Engineering (Medical Image and Data Processing). During my master's study, I have developed my basic understanding of deep learning, pattern recognition, and analysis. I actively participated in a deep learning exercise course in which I constructed fundamental neural network architectures such as FCNN, CNN, and RNN. This hands-on experience greatly contributed to my understanding of these model structures. I have worked on several image processing and deep learning projects and improved my programming skills in Python and C++." << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'I have worked on a research project where we used CycleGAN and Segmentator models to translate digital fundus images to fluorescein angiography (FA) images, and vice versa. We focused on preserving the shape of the retinal vessels during translation. We implemented our model in the PyTorch framework. This project gave me practical experience to work with actual medical dataset and different deep learning models.' << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'During my master’s study, I worked as a student research assistant at FAPS - Institute for Factory Automation and Production Systems, Erlangen. I was responsible for providing technical assistance to research teams in the development of automation solutions. This role provided me with the chance to enhance my analytical and programming skills. I have also wrote my master’s thesis at FAPS-Erlangen. The topic is “High-Precision Calibration of Multi-Camera Systems” (https://github.com/TabassumNova/Multi_Camera_Calibration). For multi-camera calibration framework, I have employed state of art methods with modification to enhance the system\'s robustness, particularly in situations where camera views do not overlap. I have also developed a PyQt user interface to analyze the calibration quality. ' << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'I most like the part of my job where I can play with different hardware and implement my knowledge about computer vision, machine learning, and data analysis to solve real-world problems. I also enjoy learning new things and adding depth to my knowledge. In the future, I want to explore more in the challenging fields of computer vision and deep learning and their practical application in automation.' << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'The Software Engineer, Probabilistic Robotics Applications (DE) at Signaloid is a perfect opportunity for me to delve deeper into the realm of machine learning and data science. I will have the opportunity to take new challenges and learn new skills. This program will also offer a gateway to expand my professional network by collaborating with pioneering researchers. With all this in mind, I see this opportunity to align my choice with my objective in terms of career progression.' << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'I hope that you will find me eligible for this opportunity. Thank you for your time. I hope to hear from you soon.' << std::endl;
+	std::cout << '\n' << std::endl;
+	std::cout << 'Sincerely,' << std::endl;
+	std::cout << 'Tasnim Tabassum Nova' << std::endl;
+	
+	
+	// 	std::vector<double> vec {
 // 		UxHwDoubleUniformDist(0.0, 1.0),
 // 		UxHwDoubleUniformDist(1.0, 2.0),
 // 	};

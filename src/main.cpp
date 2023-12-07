adbc93f9af45dd137b5b3219a2fa899380f7fe62
TabassumNova <77242943+TabassumNova@users.noreply.github.com>
2023-12-07 14:54:24 +0600

    init_commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,33 @@
 #include <vector>
 #include <uxhw.h>
 
-int
-main(int argc, char *  argv[])
+// int
+// main(int argc, char *  argv[])
+main()
 {
-	std::vector<double> vec {
-		UxHwDoubleUniformDist(0.0, 1.0),
-		UxHwDoubleUniformDist(1.0, 2.0),
-	};
+	std::cout << "I am writing to express my interest in the Machine Learning Engineer Robotics at TruPhysics. I am confident that with my academic background, prior experience, and skills, I would be able to perform the responsibilities demanded by this position." << std::endl;
+// 	std::vector<double> vec {
+// 		UxHwDoubleUniformDist(0.0, 1.0),
+// 		UxHwDoubleUniformDist(1.0, 2.0),
+// 	};
 
-	std::cout << "Initial distributions:" << std::endl;
-	for (const auto& dist : vec)
-	{
-		std::cout << dist << std::endl;
-	}
+// 	std::cout << "Initial distributions:" << std::endl;
+// 	for (const auto& dist : vec)
+// 	{
+// 		std::cout << dist << std::endl;
+// 	}
 
-	std::adjacent_difference(vec.begin(), vec.end(), vec.begin());
+// 	std::adjacent_difference(vec.begin(), vec.end(), vec.begin());
 
-	std::cout << "Differences:" << std::endl;
-	for (const auto& diff : vec)
-	{
-		std::cout << diff << std::endl;
-	}
+// 	std::cout << "Differences:" << std::endl;
+// 	for (const auto& diff : vec)
+// 	{
+// 		std::cout << diff << std::endl;
+// 	}
 
-#ifdef DEBUG
-	std::cout << "debug message" << std::endl;
-#endif
+// #ifdef DEBUG
+// 	std::cout << "debug message" << std::endl;
+// #endif
 
 	return 0;
 }
